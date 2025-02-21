a97c64a6cb4d894b37cce7b2edd23f7ef2a13a48
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-02-21 09:55:50 +0700

    Percobaan 3

--- Jobsheet2/MahasiswaMain11.java
+++ Jobsheet2/MahasiswaMain11.java
@@ -11,5 +11,10 @@ public class MahasiswaMain11 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa11 mhs2 = new Mahasiswa11("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
+
     }
 }
