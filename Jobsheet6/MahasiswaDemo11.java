91bdba2ea0c573dd5b1dcffad35e856cbd92be86
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-03-28 13:42:39 +0700

    modifikasi

--- Jobsheet6/MahasiswaDemo11.java
+++ Jobsheet6/MahasiswaDemo11.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
 public class MahasiswaDemo11 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         MahasiswaBerprestasi11 list = new MahasiswaBerprestasi11();
-        Mahasiswa11 m1 = new Mahasiswa11("123", "Zidan", "2A", 3.2);
-        Mahasiswa11 m2 = new Mahasiswa11("124", "Ayu", "2A", 3.5);
-        Mahasiswa11 m3 = new Mahasiswa11("125", "Sofi", "2A", 3.1);
-        Mahasiswa11 m4 = new Mahasiswa11("126", "Sita", "2A", 3.9);
-        Mahasiswa11 m5 = new Mahasiswa11("127", "Miki", "2A", 3.7);
+        
+        System.out.print("Masukkan jumlah Mahasiswa: ");
+        int jmlMhs = input.nextInt();
+        input.nextLine();
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        for (int i = 0; i < jmlMhs; i++) {
+            System.out.println("Masukkan data mahasiswa ke-" + (i + 1) + ": ");
+            System.out.print("NIM: ");
+            String nim = input.nextLine();
+            System.out.print("Nama: ");
+            String nama = input.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = input.nextLine();
+            System.out.print("IPK: ");
+            double ipk = input.nextDouble();
+            input.nextLine(); 
+
+            Mahasiswa11 mhs = new Mahasiswa11(nim, nama, kelas, ipk);
+            list.tambah(mhs);
+        }
 
         System.out.println("Data mahasiswa sebelum sorting: ");
         list.tampil();
