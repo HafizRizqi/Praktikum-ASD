85537882476b394f627f8bea7f09fdcdbd427a57
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-04-08 19:34:02 +0700

    Binary Search

--- Jobsheet7/MahasiswaDemo11.java
+++ Jobsheet7/MahasiswaDemo11.java
@@ -15,15 +15,16 @@ public class MahasiswaDemo11 {
             String kelas = input.nextLine();
             System.out.print("IPK: ");
             double ipk = input.nextDouble();
-            input.nextLine();
             System.out.println("------------------------------------------"); 
+            input.nextLine();
+            
 
             Mahasiswa11 mhs = new Mahasiswa11(nim, nama, kelas, ipk);
             list.tambah(mhs);
         }
 
         list.tampil();
-        //melakukan pencarian data sequential
+        //melakukan pencarian data Binary
         System.out.println("------------------------------------------");
         System.out.println("Pencarian data");
         System.out.println("------------------------------------------");
@@ -31,10 +32,12 @@ public class MahasiswaDemo11 {
         System.out.print("IPK: ");
         double cari = input.nextDouble();
 
-        System.out.println("menggunakan sequential searching");
-        double posisi = list.sequentialSearching(cari);
-        int pss = (int) posisi;
-        list.tampilPosisi(cari, pss);
-        list.tampilDataSearch(cari, pss);
+        System.out.println("menggunakan binary search");
+        double posisi2 = list.findBinarySearch(cari, 0, jmlMhs-1);
+        int pss2 = (int) posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
+
+        
     }
 }
