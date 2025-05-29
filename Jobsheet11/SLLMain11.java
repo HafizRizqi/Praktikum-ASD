a38f097475a1dacdf7f9794ac3e332ac8db70a99
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-05-29 19:31:11 +0700

    Modifikasi

--- Jobsheet11/SLLMain11.java
+++ Jobsheet11/SLLMain11.java
@@ -1,8 +1,27 @@
 package Jobsheet11;
-
+import java.util.Scanner;
 public class SLLMain11 {
     public static void main(String[] args) {
         SingleLinkedList11 sll = new SingleLinkedList11();
+        Scanner input = new Scanner(System.in);
+        System.out.println("=== Data Mahasiswa ===");
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlah = input.nextInt();
+        input.nextLine(); 
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("NIM: ");
+            String nim = input.nextLine();
+            System.out.print("Nama: ");
+            String nama = input.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = input.nextLine();
+            System.out.print("IPK: ");
+            double ipk = input.nextDouble();
+            input.nextLine(); 
+            Mahasiswa11 mhs = new Mahasiswa11(nim, nama, kelas, ipk);
+            sll.addLast(mhs);
+
+        }
 
         Mahasiswa11 mhs1 = new Mahasiswa11("24212200", "Alvaro", "1A", 4.0);
         Mahasiswa11 mhs2 = new Mahasiswa11("24212201", "Bimon", "2B", 3.8);
