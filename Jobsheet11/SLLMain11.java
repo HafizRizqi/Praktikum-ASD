a5b12f53e794e5f24c8d536c45565a81e587cc84
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-05-29 20:12:04 +0700

    Praktikum2

--- Jobsheet11/SLLMain11.java
+++ Jobsheet11/SLLMain11.java
@@ -1,28 +1,8 @@
 package Jobsheet11;
-import java.util.Scanner;
+
 public class SLLMain11 {
     public static void main(String[] args) {
         SingleLinkedList11 sll = new SingleLinkedList11();
-        Scanner input = new Scanner(System.in);
-        System.out.println("=== Data Mahasiswa ===");
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int jumlah = input.nextInt();
-        input.nextLine(); 
-        for (int i = 0; i < jumlah; i++) {
-            System.out.print("NIM: ");
-            String nim = input.nextLine();
-            System.out.print("Nama: ");
-            String nama = input.nextLine();
-            System.out.print("Kelas: ");
-            String kelas = input.nextLine();
-            System.out.print("IPK: ");
-            double ipk = input.nextDouble();
-            input.nextLine(); 
-            Mahasiswa11 mhs = new Mahasiswa11(nim, nama, kelas, ipk);
-            sll.addLast(mhs);
-
-        }
-
         Mahasiswa11 mhs1 = new Mahasiswa11("24212200", "Alvaro", "1A", 4.0);
         Mahasiswa11 mhs2 = new Mahasiswa11("24212201", "Bimon", "2B", 3.8);
         Mahasiswa11 mhs3 = new Mahasiswa11("24212202", "Cintia", "3C", 3.5);
@@ -37,6 +17,18 @@ public class SLLMain11 {
         sll.insertAfter("Dirga", mhs3);
         sll.print();
         sll.insertAt(2, mhs2);
+        sll.print();
+
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("Bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
         sll.print();
     }
 }
