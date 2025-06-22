0f2e81561653cf158ba510e18b6ddd2e4a7b6bc3
Hafiz Rizqi Hernanda <hafizhernanda5@gmail.com>
2025-06-23 01:00:48 +0700

    Praktikum 2

--- Jobsheet14/BinaryTreeMain11.java
+++ Jobsheet14/BinaryTreeMain11.java
@@ -2,40 +2,21 @@ package Jobsheet14;
 
 public class BinaryTreeMain11 {
     public static void main(String[] args) {
-        BinaryTree11 bat = new BinaryTree11();
+        BinaryTreeArray11 bta = new BinaryTreeArray11();
 
-        bat.add(new Mahasiswa11("244160121", "Ali", "A", 3.57));
-        bat.add(new Mahasiswa11("244160121", "Badar", "B", 3.85));
-        bat.add(new Mahasiswa11("244160185", "Candra", "C", 3.21));
-        bat.add(new Mahasiswa11("244160220", "Dewi", "D", 3.54));
+        Mahasiswa11 mhs1 = new Mahasiswa11("244160121", "Ali", "A", 3.57);
+        Mahasiswa11 mhs2 = new Mahasiswa11("244160185", "Candra", "C", 3.41);
+        Mahasiswa11 mhs3 = new Mahasiswa11("244160221", "Badar", "B", 3.75);
+        Mahasiswa11 mhs4 = new Mahasiswa11("244160220", "Dewi", "B", 3.35);
+        Mahasiswa11 mhs5 = new Mahasiswa11("244160131", "Devi", "A", 3.48);
+        Mahasiswa11 mhs6 = new Mahasiswa11("244160205", "Ehsan", "D", 3.61);
+        Mahasiswa11 mhs7 = new Mahasiswa11("244160170", "Fizi", "B", 3.86);
 
-        System.out.println("\nDaftar nama mahasiswa(in Order Traversal):");
-        bat.traversalInOrder(bat.root);
-
-        System.out.println("\nPencarian data mahasiswa:");
-        System.out.print("Cari mahasiswa dengan ipk: 3.54 : ");
-        String hasilCari = bat.find(3.54) ? "Ditemukan" : "Tidak ditemukan";
-        System.out.println(hasilCari);
-
-        System.out.print("Cari mahasiswa dengan ipk: 3.22 : ");
-        hasilCari = bat.find(3.22) ? "Ditemukan" : "Tidak ditemukan";
-        System.out.println(hasilCari);
-
-        bat.add(new Mahasiswa11("244160131", "Devi", "A", 3.72));
-        bat.add(new Mahasiswa11("244160205", "Ehsan", "D", 3.37));
-        bat.add(new Mahasiswa11("244160170", "Fizi", "B", 3.46));
-        System.out.println("\nDaftar semua mahasiswa setelah penambahan 3 mahasiswa:");
-        System.out.println("\nInOrder Traversal:");
-        bat.traversalInOrder(bat.root);
-        System.out.println("\nPreOrder Traversal:");
-        bat.traversalPreOrder(bat.root);
-        System.out.println("\nPostOrder Traversal:");
-        bat.traversalPostOrder(bat.root);
-
-        System.out.println("\npenghapusan data mahasiswa");
-        bat.delete(3.57);
-        System.out.println("\nDaftar mahasiswa setelah penghapusan 1 mahasiswa (in Order):");
-        bat.traversalInOrder(bat.root);
+        Mahasiswa11[] dataMahasiswa = {mhs1, mhs2, mhs3, mhs4, mhs5, mhs6, mhs7, null, null, null};
+        int idxLast = 6;
+        bta.populateData(dataMahasiswa, idxLast);
+        System.out.println("\nInOrder Traversal Mahasiswa: ");
+        bta.traverseInOrder(0);
     }
 
 
